6b0fce876b92eef9ad597d54210e4f5736f63d70
Nelson Wei <nelson@ensemble.com>
2018-09-14 11:34:49 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import React, { Component } from "react";
+import "./App.css";
+import NavBar from "./components/navbar";
+import Counters from "./components/counters";
+
+class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 }
+    ]
+  };
+
+  constructor() {
+    super();
+    console.log("App - constructor");
+    // this.state = this.props.something;
+  }
+
+  componentDidMount() {
+    // Make ajax calls to get data from server
+    console.log("App - mounted");
+  }
+
+  handleIncrement = counter => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    console.log(this.state.counters[index]);
+
+    this.setState({ counters });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+    this.setState(counters);
+  };
+
+  handleDelete = counterId => {
+    // console.log("Event Handler Called", counterId);
+    const counters = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({ counters });
+  };
+
+  render() {
+    console.log("App - rendered");
+    return (
+      <React.Fragment>
+        <NavBar
+          totalCounters={this.state.counters.filter(c => c.value > 0).length}
+        />
+        <main className="container">
+          <Counters
+            onDelete={this.handleDelete}
+            onIncrement={this.handleIncrement}
+            onReset={this.handleReset}
+            counters={this.state.counters}
+          />
+        </main>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
